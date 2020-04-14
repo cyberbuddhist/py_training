442d730e0bc571c2204ff03075f0592c2317b4f1
cyberbuddhist <ber00t@yandex.ru>
2020-04-15 00:41:24 +0300

    В код добавлены комментарии

--- test_python_group.py
+++ test_python_group.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class TestPythonGroup(unittest.TestCase):
     def setUp(self):
@@ -14,7 +11,9 @@ class TestPythonGroup(unittest.TestCase):
 
     def test_python_group(self):
         wd = self.wd
+        # open home page
         wd.get("http://localhost/addressbook/group.php")
+        # login
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_id("LoginForm").click()
@@ -22,7 +21,9 @@ class TestPythonGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open groups page and init group creation
         wd.find_element_by_name("new").click()
+        #fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("Testgroup")
@@ -32,8 +33,11 @@ class TestPythonGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("Ijsdjhfjdhb")
+        # submit group creation
         wd.find_element_by_name("submit").click()
+        # return to groups page
         wd.find_element_by_link_text("group page").click()
+        # logout
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
